82cdebf18a214569aea6cc6cc68ff45453edcde7
wenc <wen-c@aeonpeople.biz>
2020-04-30 13:55:00 +0800

    web 引入swagger

--- tester-webapp/src/main/java/com/tester/testerwebapp/model/IdAndNameModel.java
+++ tester-webapp/src/main/java/com/tester/testerwebapp/model/IdAndNameModel.java
@@ -1,15 +1,18 @@
 package com.tester.testerwebapp.model;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.experimental.Accessors;
-
 import javax.validation.constraints.Email;
-import javax.validation.constraints.Pattern;
 
+@ApiModel(description = "id和名字VO")
 @Data
 @Accessors(chain = true)
 public class IdAndNameModel {
+    @ApiModelProperty(value = "id", example = "1209",required = true)
     private Long id;
+    @ApiModelProperty(value = "名称", example = "看看",required = true)
     private String name;
 //    @Email(message = "invalid email",regexp = "^([a-z0-9A-Z]+[-|\\.]?)+[a-z0-9A-Z]@([a-z0-9A-Z]+(-[a-z0-9A-Z]+)?\\.)+[a-zA-Z]{2,}$")
     @Email(message = "invalid email",regexp = "^(\\w+?)@(\\w+?)\\.([a-zA-Z]{2,})$")
