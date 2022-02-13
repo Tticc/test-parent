9a9729c15ea7f5f67ecb247b5363f408e0c3d16f
Tticc <18883394480@163.com>
2022-02-13 17:34:57 +0800

    web 多线程执行

--- tester-cv/src/test/java/com/tester/testercv/img/start/ImgDownload_Async.java
+++ tester-cv/src/test/java/com/tester/testercv/img/start/ImgDownload_Async.java
@@ -4,6 +4,7 @@ import com.tester.testercommon.util.http.HttpsClient;
 import com.tester.testercommon.util.pool.DefaultPool;
 import lombok.Data;
 import lombok.experimental.Accessors;
+import lombok.extern.slf4j.Slf4j;
 
 import java.io.File;
 import java.net.Proxy;
@@ -11,6 +12,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.LockSupport;
 import java.util.function.Consumer;
 
@@ -18,14 +20,19 @@ import java.util.function.Consumer;
  * @Author 温昌营
  * @Date 2021-11-10 09:40:14
  */
-public class ImgDownload {
+@Slf4j
+public class ImgDownload_Async {
 
 
     private static BlockingQueue<DownloadTask> queue = new LinkedBlockingQueue<>();
+    private static BlockingQueue<DownloadImgTask> imgQueue = new LinkedBlockingQueue<>();
 
     private static Thread monitorThread;
 
     private static Proxy proxy = HttpsClient.getProxy("127.0.0.1", 10809);
+
+    private static boolean async = false;
+
 
     /**
      * main方法启动
@@ -34,8 +41,9 @@ public class ImgDownload {
      * @Author 温昌营
      **/
     public static void main(String[] args) throws Exception {
-        ImgDownload imgDownload = new ImgDownload();
-        imgDownload.start(DefaultPool::exec);
+        Consumer<Runnable> taskConsumer = DefaultPool::exec;
+        ImgDownload_Async imgDownload = new ImgDownload_Async();
+        imgDownload.start(taskConsumer);
         Map<String, String> propertyMap = new HashMap<>();
         propertyMap.put("user-agent","Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36");
 
@@ -43,7 +51,9 @@ public class ImgDownload {
                 199,
                 "http://www.xxx.xx.com",
                 "C:\\Users\\wenc\\Desktop\\captureImg\\download\\img\\pic8\\",
-                propertyMap);
+                propertyMap,
+                taskConsumer);
+        async = true;
 
     }
 
@@ -56,6 +66,11 @@ public class ImgDownload {
     public void start(Consumer<Runnable> consumer) {
         // 启动一个线程，避免main退出后，下面的守护线程因为处于sleep状态而未能拉取任务开启新线程，进而只剩下守护线程，jvm退出
         consumer.accept(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(5);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         });
         MonitorTask monitorTask = new MonitorTask(consumer);
         Thread thread = new Thread(monitorTask, "my-monitor-deamon");
@@ -70,7 +85,7 @@ public class ImgDownload {
      * @Date 18:04 2021/11/10
      * @Author 温昌营
      **/
-    public void pushTask(int startIndex, int endIndex, String prefixUrl, String folderPath,Map<String, String> propertyMap) {
+    public void pushTask(int startIndex, int endIndex, String prefixUrl, String folderPath,Map<String, String> propertyMap, Consumer<Runnable> consumer) {
         File dir = new File(folderPath);
         if(!dir.exists()){
             if(dir.mkdirs()){
@@ -86,7 +101,8 @@ public class ImgDownload {
                 .setPrefixUrl(prefixUrl)
 //                .setPostfixUrl(oriTask.getPostfixUrl())
                 .setPrefixFilePath(folderPath)
-                .setPropertyMap(propertyMap);
+                .setPropertyMap(propertyMap)
+                .setConsumer(consumer);
         putEle(downloadTask);
     }
 
@@ -127,6 +143,7 @@ public class ImgDownload {
     @Data
     @Accessors(chain = true)
     public static class DownloadTask implements Runnable {
+        private Consumer<Runnable> consumer;
         private int startIndex;
         private int endIndex;
         private int currentIndex;
@@ -142,11 +159,12 @@ public class ImgDownload {
                     .setCurrentIndex(currentIndex)
                     .setPrefixUrl(oriTask.getPrefixUrl())
                     .setPostfixUrl(oriTask.getPostfixUrl())
-                    .setPrefixFilePath(oriTask.getPrefixFilePath());
+                    .setPrefixFilePath(oriTask.getPrefixFilePath())
+                    .setConsumer(oriTask.getConsumer());
         }
 
-        @Override
-        public void run() {
+//        @Override
+        public void run_ori() {
             try {
                 while (this.currentIndex <= this.endIndex) {
                     String url = getUrl(this.prefixUrl, this.currentIndex, this.postfixUrl);
@@ -157,6 +175,60 @@ public class ImgDownload {
             } catch (Exception e) {
                 putEle(this);
             }
+        }
+
+        @Override
+        public void run() {
+            log.info("分配任务线程启动。");
+            while (this.currentIndex <= this.endIndex) {
+                String url = getUrl(this.prefixUrl, this.currentIndex, this.postfixUrl);
+                String fileName = getFileName(this.prefixFilePath, this.currentIndex, ".jpg");
+                DownloadImgTask downloadImgTask = new DownloadImgTask();
+                downloadImgTask.setFilePath(fileName).setUrl(url).setPropertyMap(propertyMap);
+                putDownloadImgTask(downloadImgTask);
+                ++this.currentIndex;
+            }
+            log.info("分配任务线程执行完成。");
+            DownloadImgTask poll_imgTask = null;
+            boolean starting = true;
+            while(starting){
+                try {
+                    poll_imgTask = imgQueue.poll(20,TimeUnit.SECONDS);
+                } catch (InterruptedException e) {
+                    starting = false;
+                }
+                if(async){
+                    consumer.accept(poll_imgTask);
+                }else {
+                    poll_imgTask.run();
+                }
+            }
+        }
+    }
+
+    @Data
+    @Accessors(chain = true)
+    public static class DownloadImgTask implements Runnable {
+        private String url;
+        private String filePath;
+        Map<String, String> propertyMap = null;
+        @Override
+        public void run() {
+            try {
+                File outFile = new File(filePath);
+                HttpsClient.requestForFile(url, HttpsClient.GET_METHOD, null, propertyMap, outFile, proxy);
+            } catch (Exception e) {
+                putDownloadImgTask(this);
+            }
+        }
+    }
+
+    public static void putDownloadImgTask(DownloadImgTask task) {
+        boolean offer = imgQueue.offer(task);
+        if(offer){
+            log.info("异常download重新入队成功。{}",task.getUrl());
+        }else{
+            log.error("异常download重新入队失败。{}",task.getUrl());
         }
     }
 
