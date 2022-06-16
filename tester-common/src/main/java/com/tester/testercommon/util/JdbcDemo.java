9084b03d5a50ff10ed602e259f9c8c9ec293e4f4
wenc <wen-c@aeonpeople.biz>
2022-06-16 14:59:26 +0800

    tester common jdbc demo

--- tester-common/src/main/java/com/tester/testercommon/util/JdbcDemo.java
+++ tester-common/src/main/java/com/tester/testercommon/util/JdbcDemo.java
@@ -1,19 +1,31 @@
 package com.tester.testercommon.util;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.List;
 
 /**
+ * jdbc 和 spring-boot-starter-data-jdbc案例
  * @Author 温昌营
  * @Date 2022-6-16 14:54:18
  */
 @Slf4j
 public class JdbcDemo {
 
+    /**
+     * jdbc
+     * @Date 14:57 2022/6/16
+     * @Author 温昌营
+     **/
     public static void getConnection() throws Exception {
         Connection connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/test_parent?useSSL=false&useUnicode=true&characterEncoding=utf-8&allowMultiQueries=true&serverTimezone=GMT%2B8&allowPublicKeyRetrieval=true","root", "123456");
         PreparedStatement pstm =connection.prepareStatement("select * from u_person where `name` = ? order by id limit 2");
@@ -27,4 +39,20 @@ public class JdbcDemo {
                     ,rs.getString("cellphone"));
         }
     }
+
+    // spring-boot-starter-data-jdbc 案例
+//
+//    // 数据源组件
+//    @Autowired
+//    DataSource dataSource;
+//
+//    // 用于访问数据库的组件
+//    @Autowired
+//    JdbcTemplate jdbcTemplate;
+//
+//    @GetMapping("/getData")
+//    public List<UserDomain> getData() {
+//        List<UserDomain> query = jdbcTemplate.query("SELECT * FROM `u_person` limit 3", new BeanPropertyRowMapper<>(UserDomain.class));
+//        return query;
+//    }
 }
