037bad8dc5bb8eb55b94c3a3d43fd27de9762e70
wenc <wen-c@aeonpeople.biz>
2022-08-01 16:51:41 +0800

    tester-swing 修改application名称

--- tester-swing/src/main/java/com/tester/testerswing/TesterSwingApplication.java
+++ tester-swing/src/main/java/com/tester/testerswing/TesterSwingApplication.java
@@ -7,7 +7,7 @@ import com.tester.testerswing.swing.EasyScript;
  * @Date 2022-7-30 20:51:56
  * @Author 温昌营
  **/
-public class TesterSearchApplication {
+public class TesterSwingApplication {
 
     public static void main(String[] args) {
         new EasyScript().start();
