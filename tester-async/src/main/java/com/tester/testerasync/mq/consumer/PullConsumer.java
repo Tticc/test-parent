574803533751eb7c48a4b119596e5a14c1187e5c
wenc <Dmc019222>
2022-12-28 15:30:38 +0800

    tester-async mq test

--- tester-async/src/main/java/com/tester/testerasync/mq/consumer/PullConsumer.java
+++ tester-async/src/main/java/com/tester/testerasync/mq/consumer/PullConsumer.java
@@ -1,42 +1,83 @@
-//package com.tester.testerasync.mq.consumer;
-//
-//import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
-//import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
-//import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
-//import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
-//import org.apache.rocketmq.client.exception.MQClientException;
-//import org.apache.rocketmq.common.consumer.ConsumeFromWhere;
-//import org.apache.rocketmq.common.message.MessageExt;
-//import org.apache.rocketmq.common.protocol.heartbeat.MessageModel;
-//
-//import java.util.List;
-//
-///**
-// * DefaultLitePullConsumer需要的版本高于4.5.1，暂时废弃
-// */
-//public class PullConsumer {
-//    public static void main(String[] args) throws Exception {
-//        DefaultLitePullConsumer consumer = new DefaultLitePullConsumer("my_cg");
-//
-//        // 指定那么server
-//        consumer.setNamesrvAddr("localhost:9876");
-//        // 指定从第一条消息开始消费
-//        consumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_FIRST_OFFSET);
-//        // 指定topic与tag
-//        consumer.subscribe("someT", "*");
-//        // 设置消费模式，默认为 集群模式
-//        consumer.setMessageModel(MessageModel.CLUSTERING);
-//
-//        consumer.registerMessageListener(new MessageListenerConcurrently() {
-//            @Override
-//            public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> msgs, ConsumeConcurrentlyContext context) {
-//                for (MessageExt msg:msgs){
-//                    System.out.println(msg+ " , content : "+ new String(msg.getBody()));
-//                }
-//                return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
-//            }
-//        });
-//        consumer.start();
-//        System.out.println("consumer started");
-//    }
-//}
+package com.tester.testerasync.mq.consumer;
+
+import org.apache.rocketmq.client.consumer.DefaultMQPullConsumer;
+import org.apache.rocketmq.client.consumer.PullResult;
+import org.apache.rocketmq.common.message.MessageQueue;
+import org.apache.rocketmq.common.protocol.heartbeat.MessageModel;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * DefaultLitePullConsumer需要的版本高于4.5.1，暂时废弃
+ */
+public class PullConsumer {
+    // 记录每个队列的消费进度
+    private static final Map<MessageQueue, Long> OFFSE_TABLE = new HashMap<MessageQueue, Long>();
+
+    public static void main(String[] args) throws Exception {
+        DefaultMQPullConsumer consumer = new DefaultMQPullConsumer("my_cg");
+        // 指定那么server
+        consumer.setNamesrvAddr("localhost:9876");
+        // 设置消费模式，默认为 集群模式
+        consumer.setMessageModel(MessageModel.CLUSTERING);
+        consumer.start();
+        Set<MessageQueue> mqs = consumer.fetchSubscribeMessageQueues("topic");
+
+        for (MessageQueue mq : mqs) {
+            System.out.printf("Consume from the queue: %s%n", mq);
+            SINGLE_MQ:
+            while (true) {
+                try {
+                    // 5. 拉取消息，arg1=消息队列，arg2=tag消息过滤，arg3=消息队列，arg4=一次最大拉去消息数量
+                    PullResult pullResult =
+                            consumer.pullBlockIfNotFound(mq, null, getMessageQueueOffset(mq), 32);
+                    System.out.printf("%s%n", pullResult);
+                    // 6. 将消息放入hash表中，存储该队列的消费进度
+                    putMessageQueueOffset(mq, pullResult.getNextBeginOffset());
+                    switch (pullResult.getPullStatus()) {
+                        case FOUND:  // 找到消息，输出
+                            System.out.println(pullResult.getMsgFoundList().get(0));
+                            break;
+                        case NO_MATCHED_MSG:  // 没有匹配tag的消息
+                            System.out.println("无匹配消息");
+                            break;
+                        case NO_NEW_MSG:  // 该队列没有新消息，消费offset=最大offset
+                            System.out.println("没有新消息");
+                            break SINGLE_MQ;  // 跳出该队列遍历
+                        case OFFSET_ILLEGAL:  // offset不合法
+                            System.out.println("Offset不合法");
+                            break;
+                        default:
+                            break;
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        // 7. 关闭Consumer
+        consumer.shutdown();
+        System.out.println("consumer started");
+    }
+
+    private static long getMessageQueueOffset(MessageQueue mq) {
+        Long offset = OFFSE_TABLE.get(mq);
+        if (offset != null)
+            return offset;
+
+        return 0;
+    }
+
+    /**
+     * 将消费进度更新到Hash表
+     *
+     * @param mq     消息队列
+     * @param offset offset
+     */
+    private static void putMessageQueueOffset(MessageQueue mq, long offset) {
+        OFFSE_TABLE.put(mq, offset);
+    }
+}
--- tester-async/src/main/java/com/tester/testerasync/mq/producer/msgType/OrderedProducer.java
+++ tester-async/src/main/java/com/tester/testerasync/mq/producer/msgType/OrderedProducer.java
@@ -24,6 +24,10 @@ public class OrderedProducer {
             msg.setKeys(orderId.toString());
             // send方法的第三个参数orderId，将会传递给内部类select方法的第三个参数
             SendResult sendResult = producer.send(msg, new MessageQueueSelector() {
+                // 第一个参数的mqs是集群所有broker的所有queue吗？
+                // 如果顺序消息发送期间恰好有个broker下线了，且producer感知到了，那么mqs数量是不是发生了变化？
+                // 变化之后取模不一样了，发到了不同的queue？
+                // 是的，发到其他queue，因此会乱序
                 @Override
                 public MessageQueue select(List<MessageQueue> mqs, Message msg, Object arg) {
                     Integer id = (Integer)arg;
